68b961ea7beba71077454911667b8843aa86bbaf
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-07-12 16:39:29 +0900

    Update `swc_core` to `v0.96.9` (#67378)
    * https://github.com/vercel/turbo/pull/8686 <!-- Tobias Koppers -
    improve performance of the graph aggregation -->
    * https://github.com/vercel/turbo/pull/8694 <!-- Benjamin Woodruff -
    Replace `unreachable` with `bail` in `finalize_css` -->
    * https://github.com/vercel/turbo/pull/8661 <!-- Benjamin Woodruff -
    Create a minimum viable `ResolvedVc` type -->
    * https://github.com/vercel/turbo/pull/8662 <!-- Benjamin Woodruff - Add
    a minimum viable ResolvedValue marker trait -->
    * https://github.com/vercel/turbo/pull/8678 <!-- Benjamin Woodruff - Add
    derive macro for ResolvedValue -->
    * https://github.com/vercel/turbo/pull/8715 <!-- Benjamin Woodruff -
    Switch RcStr from std::sync::Arc to triomphe::Arc -->
    * https://github.com/vercel/turbo/pull/8699 <!-- Donny/강동윤 - perf: Merge
    multiple `EsmBinding` -->
    * https://github.com/vercel/turbo/pull/8720 <!-- Benjamin Woodruff - Add
    support for `#[turbo_tasks::value(resolved)]` and
    `#[turbo_tasks::value_trait(resolved)]` -->
    * https://github.com/vercel/turbo/pull/8706 <!-- Donny/강동윤 - build:
    Update `swc_core` to `v0.96.9` -->
    
    Update SWC crates to
    https://github.com/swc-project/swc/commit/226617e73edb2070ea1d4be2da93239b3f3ebc88
    and
    https://github.com/swc-project/plugins/commit/b7658c3153b1f1e3c941bc0f6bdae6856f5d278c
    
    To keep in sync
    
     - Closes https://github.com/vercel/next.js/issues/64890
     - Closes https://github.com/vercel/next.js/issues/63104

--- test/development/basic/hmr.test.ts
+++ test/development/basic/hmr.test.ts
@@ -16,7 +16,7 @@ import { createNext } from 'e2e-utils'
 import { NextInstance } from 'e2e-utils'
 import { outdent } from 'outdent'
 
-describe.each([[''], ['/docs']])(
+describe.each([['', '/docs']])(
   'basic HMR, basePath: %p',
   (basePath: string) => {
     let next: NextInstance
@@ -224,9 +224,8 @@ describe.each([[''], ['/docs']])(
             try {
               // Check whether the this page has reloaded or not.
               await check(async () => {
-                const editedPTag = await browser.elementByCss(
-                  '.hmr-style-page p'
-                )
+                const editedPTag =
+                  await browser.elementByCss('.hmr-style-page p')
                 return editedPTag.getComputedCss('font-size')
               }, /200px/)
             } finally {
@@ -319,9 +318,8 @@ describe.each([[''], ['/docs']])(
               basePath + '/hmr/style-dynamic-component'
             )
             // Check whether the this page has reloaded or not.
-            const editedDiv = await secondBrowser.elementByCss(
-              '#dynamic-component'
-            )
+            const editedDiv =
+              await secondBrowser.elementByCss('#dynamic-component')
             const editedClientClassName = await editedDiv.getAttribute('class')
             const editedFontSize = await editedDiv.getComputedCss('font-size')
             const browserHtml = await secondBrowser.eval(
@@ -499,30 +497,28 @@ describe.each([[''], ['/docs']])(
         )
         if (basePath === '' && !process.env.TURBOPACK) {
           expect(source).toMatchInlineSnapshot(`
-            "./pages/hmr/about2.js
-            Error: 
-              x Unexpected token. Did you mean \`{'}'}\` or \`&rbrace;\`?
-               ,-[TEST_DIR/pages/hmr/about2.js:4:1]
-             4 |       <p>This is the about page.</p>
-             5 |     div
-             6 |   )
-             7 | }
-               : ^
-               \`----
+          "./pages/hmr/about2.js
+          Error:   x Unexpected token. Did you mean \`{'}'}\` or \`&rbrace;\`?
+             ,-[7:1]
+           4 |       <p>This is the about page.</p>
+           5 |     div
+           6 |   )
+           7 | }
+             : ^
+             \`----
+            x Unexpected eof
+             ,-[7:3]
+           5 |     div
+           6 |   )
+           7 | }
+             \`----
 
-              x Unexpected eof
-               ,-[TEST_DIR/pages/hmr/about2.js:5:1]
-             5 |     div
-             6 |   )
-             7 | }
-               \`----
+          Caused by:
+              Syntax Error
 
-            Caused by:
-                Syntax Error
-
-            Import trace for requested module:
-            ./pages/hmr/about2.js"
-          `)
+          Import trace for requested module:
+          ./pages/hmr/about2.js"
+        `)
         } else if (basePath === '' && process.env.TURBOPACK) {
           expect(source).toMatchInlineSnapshot(`
             "./pages/hmr/about2.js:7:1
@@ -537,30 +533,28 @@ describe.each([[''], ['/docs']])(
           `)
         } else if (basePath === '/docs' && !process.env.TURBOPACK) {
           expect(source).toMatchInlineSnapshot(`
-            "./pages/hmr/about2.js
-            Error: 
-              x Unexpected token. Did you mean \`{'}'}\` or \`&rbrace;\`?
-               ,-[TEST_DIR/pages/hmr/about2.js:4:1]
-             4 |       <p>This is the about page.</p>
-             5 |     div
-             6 |   )
-             7 | }
-               : ^
-               \`----
+          "./pages/hmr/about2.js
+          Error:   x Unexpected token. Did you mean \`{'}'}\` or \`&rbrace;\`?
+             ,-[7:1]
+           4 |       <p>This is the about page.</p>
+           5 |     div
+           6 |   )
+           7 | }
+             : ^
+             \`----
+            x Unexpected eof
+             ,-[7:3]
+           5 |     div
+           6 |   )
+           7 | }
+             \`----
 
-              x Unexpected eof
-               ,-[TEST_DIR/pages/hmr/about2.js:5:1]
-             5 |     div
-             6 |   )
-             7 | }
-               \`----
+          Caused by:
+              Syntax Error
 
-            Caused by:
-                Syntax Error
-
-            Import trace for requested module:
-            ./pages/hmr/about2.js"
-          `)
+          Import trace for requested module:
+          ./pages/hmr/about2.js"
+        `)
         } else if (basePath === '/docs' && process.env.TURBOPACK) {
           expect(source).toMatchInlineSnapshot(`
             "./pages/hmr/about2.js:7:1
@@ -974,18 +968,17 @@ describe.each([[''], ['/docs']])(
 
             expect(next.normalizeTestDirContent(redboxSource))
               .toMatchInlineSnapshot(`
-              "./components/parse-error.js
-              Error: 
-                x Expression expected
-                 ,-[./components/parse-error.js:1:1]
-               1 | This
-               2 | is
-               3 | }}}
-                 : ^
-               4 | invalid
-               5 | js
-                 "
-            `)
+            "./components/parse-error.js
+            Error:   x Expression expected
+               ,-[3:1]
+             1 | This
+             2 | is
+             3 | }}}
+               : ^
+             4 | invalid
+             5 | js
+               "
+          `)
           }
 
           await next.patchFile(aboutPage, aboutContent)
